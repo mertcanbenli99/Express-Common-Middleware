4077396145b9881e6d2a76b391234462eb987138
Mert Can Benli <mertcanbenli99@outlook.com>
2022-12-22 12:23:49 +0300

    updates

--- src/index.ts
+++ src/index.ts
@@ -18,4 +18,4 @@ export * from "./events/ticket-updated-event";
 
 export * from "./events/types/order-status";
 export * from "./events/order-cancelled-event";
-export * from "./events/order-cancelled-event";
+export * from "./events/order-created-event";
